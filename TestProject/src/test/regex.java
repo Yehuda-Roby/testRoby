5bed10bf4cbd2caebd3ec9ca8baeaf7f2c7647c2
Yehuda-Roby <yehuda@roby.ai>
2021-08-11 16:54:16 +0300

    First commit

---
+++ TestProject/src/test/regex.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.util.regex.*;
+
+public class regex 
+{		
+	public static void main(String[] args)
+	{
+		Pattern p = Pattern.compile(".s");
+		System.out.println(System.getenv("USERNAME"));
+	}	
+}
